0b6ee6616fda4de43ed870cec81996371479379d
gztes <217619891+gztes@users.noreply.github.com>
2025-12-16 12:15:01 -0500

    fix: simplify metro.config.js for NativeWind v4 compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// NativeWind v4 configuration
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('nativewind/metro/babel'),
+};
+
+module.exports = config;
